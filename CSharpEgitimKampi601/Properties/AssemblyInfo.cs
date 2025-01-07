a5a4ed358831eee9af6544cdcccefcb781646a40
semanurmutlu <semanurmutlu29@gmail.com>
2025-01-07 17:27:42 +0300

    Add project files.

---
+++ CSharpEgitimKampi601/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSharpEgitimKampi601")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpEgitimKampi601")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2fa0394a-94e3-45db-898b-08423d01b769")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSharpEgitimKampi601/Services/CustomerOperations.cs
@@ -0,0 +1,76 @@
+﻿using CSharpEgitimKampi601.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi601.Services
+{
+    public class CustomerOperations
+    {
+        public void AddCustomer(Customer customer) 
+        {
+            var connection = new MongoDbConnection();//Bağlantı isteğinde bulundum
+            var customerCollection = connection.GetCustomersCollection();//Tabloma bağlandım
+
+            var document = new BsonDocument
+            {
+                {"CustomerName",customer.CustomerName },
+                {"CustomerSurname",customer.CustomerSurname },
+                {"CustomerCity", customer.CustomerCity},
+                {"CustomerBalance",customer.CustomerBalance },
+                {"CustomerShoppingCount",customer.CustomerShoppingCount }
+            };//Parametrelerimi gönderdim
+
+            customerCollection.InsertOne(document);//Ekleme işlemimi yaptım, Bu işlem MongoDb de bizim ekleme işlemlerimizi gerçekleştirecektir        
+        } 
+
+        public List<Customer> GetAllCustomer() //Customer türünde bütün müşterileri getir adında bir metot yazıyorum 
+        { 
+            var connection = new MongoDbConnection();//Veri tabanıma Bağlantımı oluşturuyorum
+            var customerCollection= connection.GetCustomersCollection();//customer sınıfından değerlerimi getiriyorum(kollleksiyona yani tabloya bağlantı hazırlamış oldum)
+            var customers = customerCollection.Find(new BsonDocument()).ToList();
+            List<Customer> customerList=new List<Customer>();
+            foreach (var c in customers) 
+            {
+                customerList.Add(new Customer
+                {
+                    CustomerId = c["_id"].ToString(),
+                    CustomerBalance = decimal.Parse(c["CustomerBalance"].ToString()),
+                    CustomerCity = c["CustomerCity"].ToString(),
+                    CustomerName = c["CustomerName"].ToString(),
+                    CustomerShoppingCount = int.Parse(c["CustomerShoppingCount"].ToString()),
+                    CustomerSurname = c["CustomerSurname"].ToString()
+                });
+            }
+            return customerList;
+
+        }
+        
+        public void DeleteCustomer(string id)
+        {
+            var connection = new MongoDbConnection();
+            var customerCollection = connection.GetCustomersCollection();
+            var filter=Builders<BsonDocument>.Filter.Eq("_id",ObjectId.Parse(id));
+            customerCollection.DeleteOne(filter);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            var connection = new MongoDbConnection();
+            var customerCollection = connection.GetCustomersCollection();
+            var filter = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(customer.CustomerId));
+            var updatedValue = Builders<BsonDocument>.Update
+                .Set("CustomerName", customer.CustomerName)
+                .Set("CustomerSurname", customer.CustomerSurname)
+                .Set("CustomerCity", customer.CustomerCity)
+                .Set("CustomerBalance", customer.CustomerBalance)
+                .Set("CustomerSoppingCount", customer.CustomerShoppingCount);
+            customerCollection.UpdateOne(filter,updatedValue);
+        }
+    
+    }
+}
